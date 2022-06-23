a097f31a3e4f72ed1adec5c7f18323ae89858a11
Md Mazharul Haque <cpon143@gmail.com>
2022-06-23 16:44:20 +0530

    new changes

--- script.js
+++ script.js
@@ -10,22 +10,24 @@ console.log(document.querySelector('.guess').value);
 document.querySelector('.guess').value=23;
 */
 
-const secretNumber=Math.trunc(Math.random()*20)+1;
-document.querySelector('.number').textContent=secretNumber;
+const secretNumber = Math.trunc(Math.random() * 20) + 1;
+document.querySelector('.number').textContent = secretNumber;
+let score = 20;
 
-document.querySelector('.check').addEventListener
-('click', function(){
-    const guess = Number(document.querySelector('.guess').value)
-    console.log(guess, typeof guess)
-    if(!guess){
-        document.querySelector('.message').textContent='⛔ no number!';
-    }else if(guess===secretNumber){
-        document.querySelector('.message').textContent ='🎉 Correct answer!';
-    }
-    else if(guess>secretNumber){
-        document.querySelector('.message').textContent= '📈 Too high!';
-    }
-    else if(guess<secretNumber){
-        document.querySelector('.message').textContent='📉 Too low!';
-    }
+document.querySelector('.check').addEventListener('click', function () {
+  const guess = Number(document.querySelector('.guess').value);
+  console.log(guess, typeof guess);
+  if (!guess) {
+    document.querySelector('.message').textContent = '⛔ no number!';
+  } else if (guess === secretNumber) {
+    document.querySelector('.message').textContent = '🎉 Correct answer!';
+  } else if (guess > secretNumber) {
+    document.querySelector('.message').textContent = '📈 Too high!';
+    score--;
+    document.querySelector('.score').textContent = score
+  } else if (guess < secretNumber) {
+    document.querySelector('.message').textContent = '📉 Too low!';
+    score--;
+    document.querySelector('.score').textContent = score
+  }
 });
