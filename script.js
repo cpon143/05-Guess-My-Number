b0924cac4aa55192fa97973b308d8d24881903dc
Md Mazharul Haque <cpon143@gmail.com>
2022-06-23 15:17:18 +0530

    new changes

--- script.js
+++ script.js
@@ -1,4 +1,31 @@
 'use strict';
-console.log(document.querySelector('.message').
+/*console.log(document.querySelector('.message').
 textContent);
-document.querySelector('.message').textContent ='🎉 Correct answer'
+document.querySelector('.message').textContent ='🎉 Correct answer!';
+
+document.querySelector('.number').textContent ='13';
+document.querySelector('.score').textContent = '10';
+document.querySelector('.highscore').textContent = '100';
+console.log(document.querySelector('.guess').value);
+document.querySelector('.guess').value=23;
+*/
+
+const secretNumber=Math.trunc(Math.random()*20)+1;
+document.querySelector('.number').textContent=secretNumber;
+
+document.querySelector('.check').addEventListener
+('click', function(){
+    const guess = Number(document.querySelector('.guess').value)
+    console.log(guess, typeof guess)
+    if(!guess){
+        document.querySelector('.message').textContent='⛔ no number!';
+    }else if(guess===secretNumber){
+        document.querySelector('.message').textContent ='🎉 Correct answer!';
+    }
+    else if(guess>secretNumber){
+        document.querySelector('.message').textContent= '📈 Too high';
+    }
+    else if(guess<secretNumber){
+        document.querySelector('.message').textContent='📉 Too low';
+    }
+});
