6f95418c87325e4a3c5e4c71feaf94c18a1a827b
Md Mazharul Haque <cpon143@gmail.com>
2022-06-24 16:50:39 +0530

    new changes

--- script.js
+++ script.js
@@ -11,8 +11,8 @@ document.querySelector('.guess').value=23;
 */
 
 let secretNumber = Math.trunc(Math.random() * 20) + 1;
-
 let score = 20;
+let highscore=0;
 
 document.querySelector('.check').addEventListener('click', function () {
   const guess = Number(document.querySelector('.guess').value);
@@ -29,6 +29,10 @@ document.querySelector('.check').addEventListener('click', function () {
     document.querySelector('.number').textContent = secretNumber; 
     document.querySelector('body').style.backgroundColor = '#60b347';
     document.querySelector('.number').style.width='30rem'
+    if(score>highscore){
+      highscore=score;
+      document.querySelector('.highscore').textContent=score;
+    }
   }
 
   // when guess is too high
